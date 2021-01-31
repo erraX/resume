0ce2a2686800c0bfb12f05e7084dc3feb564c9d1
erraX <niminjiecide@outlook.com>
2021-01-31 19:21:15 +0800

    update

--- src/components/OpenSourcePanel.jsx
+++ src/components/OpenSourcePanel.jsx
@@ -2,9 +2,9 @@ import Github from './Github';
 
 const sources = [
   {
-    name: 'v-rx',
-    link: 'http://github.com/errax/v-rx',
-    desc: 'vue composition hooks rxjs 封装',
+    name: 'form-after-all',
+    link: 'http://github.com/errax/form-after-all',
+    desc: 'headless form component',
   },
   {
     name: 'v-hooks',
@@ -12,34 +12,9 @@ const sources = [
     desc: 'vue composition hooks 常用功能封装',
   },
   {
-    name: 'nuxt-router-module',
-    link: 'http://github.com/errax/nuxt-router-module',
-    desc: 'nuxt 自定义路由配置插件',
-  },
-  {
     name: 'nuxt-enum-module',
     link: 'http://github.com/errax/nuxt-enum-module',
     desc: 'nuxt 枚举封装',
-  },
-  {
-    name: 'form-after-all',
-    link: 'http://github.com/errax/form-after-all',
-    desc: 'headless form component',
-  },
-  {
-    name: 'html-string-replace-webpack-plugin',
-    link: 'http://github.com/errax/html-string-replace-webpack-plugin',
-    desc: 'webpack `HtmlWebpackPlugin` 字符串替换插件',
-  },
-  {
-    name: 'inversion.js',
-    link: 'http://github.com/errax/inversion.js',
-    desc: '轻量级 IoC 容器',
-  },
-  {
-    name: 'redux-timer',
-    link: 'http://github.com/errax/redux-timer',
-    desc: 'redux 中间件，用于统计 rerender 时间',
   },
 ];
 
